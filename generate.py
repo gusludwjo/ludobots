af7755b7b17506b931e2ba114b1f7411abf333f8
gusludwjo <61656240+gusludwjo@users.noreply.github.com>
2022-02-15 14:40:26 +0100

    only one box is now generated

--- generate.py
+++ generate.py
@@ -6,24 +6,8 @@ height = 1
 x = 0
 y = 0
 z = height/2
-#pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[lenght,width, height])
-#pyrosim.Send_Cube(name="Box2", pos=[x+lenght,y,z+height] , size=[lenght,width, height])
 
-for i in range (3):
-    x=0
-    y+=1.1
-    for j in range (3):
-        lenght = 1
-        width = 1
-        height = 1
-        z= height/2
-        x+=1.1
-        for k in range (10): #box-stacking loop
-            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[lenght,width, height])
-            z+=1
-            lenght*=0.9
-            width*=0.9
-            height*=0.9
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[lenght,width, height])
 
 
 pyrosim.End()
