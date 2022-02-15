c8c0f6efdacfbfa4492833db51e93d801fb2c320
gusludwjo <61656240+gusludwjo@users.noreply.github.com>
2022-02-15 17:15:13 +0100

    Prints sensordata for backleg link

--- simulation.py
+++ simulation.py
@@ -1,10 +1,11 @@
 import pybullet as p
 import time
 import pybullet_data
+import numpy
 from generate import *
+import pyrosim.pyrosim as pyrosim
 
 Create_World()
-
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -16,9 +17,17 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(100)
 
-for i in range (10000):
+
+
+for i in range (100):
     p.stepSimulation()
     time.sleep(1./240.)
-    print (i)
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    backLegSensorValues[i] = backLegTouch
 p.disconnect()
+
+
+print (backLegSensorValues)
